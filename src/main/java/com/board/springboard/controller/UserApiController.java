103cfacc4546a12ed7c468c26e2fabd1e34d8670
minyung0723@gmail.com <minyung0723@gmail.com>
2026-03-19 10:08:05 +0900

    view controller 와 api controller 구분하여 코드 정리

--- src/main/java/com/board/springboard/controller/UserApiController.java
+++ src/main/java/com/board/springboard/controller/UserApiController.java
@@ -3,14 +3,18 @@ package com.board.springboard.controller;
 import com.board.springboard.common.CookieUtil;
 import com.board.springboard.model.dto.User;
 import com.board.springboard.model.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Map;
 
@@ -29,7 +33,7 @@ public class UserApiController {
         return ResponseEntity.ok(Map.of("duplicate", 중복)); //이메일 중복했는데 중복이 맞다면 duplicate - 중복 전달
     }
 
-    @PostMapping("/user/send-code)
+    @PostMapping("/user/send-code")
     public ResponseEntity<?> 인증번호발송(@RequestBody Map<String, String> body) {
         userService.인증번호발송(body.get("email"));
         return ResponseEntity.ok(Map.of("message", "인증번호가 발송되었습니다."));
@@ -60,16 +64,52 @@ public class UserApiController {
     }
 
     @PostMapping("/user/find-email")
-    public Map<String, String> findEmail(@RequestBody Map<String, String> requestData) {
-        User 유저데이터 = userService.이메일로유저찾기(requestData.get("email"));
-        if (유저데이터 != null) {
-            return Map.of("email", 유저데이터.getEmail());
-        } else {
-            return Map.of("error", "해당 이름으로 가입된 이메일이 없습니다.");
-        }
+    public ResponseEntity<?> findEmail(@RequestBody Map<String, String> body) {
+        User 유저데이터 = userService.이메일로유저찾기(body.get("email"));
+        if (유저데이터 != null) return ResponseEntity.ok(Map.of("email", 유저데이터.getEmail()));
+        return ResponseEntity.ok(Map.of("error", "해당 이름으로 가입된 이메일이 없습니다."));
+
+    }
+    // POST /user/profile/edit
+    // profile.jsp 의 버튼 onclick → fetch() → JSON 받아서 성공 메세지 표시
+    @PostMapping("/user/profile/edit")
+    public ResponseEntity<?> 유저정보수정(@RequestBody User user, @AuthenticationPrincipal String email) {
+
+       User 로그인유저 = userService.이메일로유저찾기(email);
+       user.setId(로그인유저.getId());
+       userService.유저정보수정(user);
+       return ResponseEntity.ok(Map.of("message","정보가 수정되었습니다."));
     }
 
-    // Todo 10 : 유저 정보 수정 (fetch() 로 요청 → JSON 응답)
+    @PostMapping("/user/logout")
+    public ResponseEntity<?> 로그아웃(@AuthenticationPrincipal String email, HttpServletResponse response){
+        userService.로그아웃(email);
+        cookieUtil.삭제(response, "access_token");
+        cookieUtil.삭제(response, "refresh_token");
+        return ResponseEntity.ok(Map.of("message", "로그아웃완료"));
+    }
+
+    @PostMapping("/user/token/refresh")
+    public ResponseEntity<?> 토큰재발급(HttpServletRequest request, HttpServletResponse response){
+        String 리프레시토큰 = cookieUtil.가져오기(request, "refresh_token");
+        if (리프레시토큰 == null) return ResponseEntity.badRequest().body(Map.of("message","다시 로그인해주세요"));
+
+        String 새액세스토큰 = userService.토큰재발급(리프레시토큰);
+        if (새액세스토큰 == null) return ResponseEntity.status(401)
+                .body(Map.of("message", "세션이 만료되었습니다. 다시 로그인해주세요."));
+
+        cookieUtil.추가(response,"access_token",새액세스토큰, 60*30);
+        return ResponseEntity.ok(Map.of("message","토큰 재발급 완료"));
+    }
+
+    @PostMapping("/user/profile/upload")
+    public ResponseEntity<?> 프로필사진업로드(@AuthenticationPrincipal String email, @RequestParam MultipartFile imageFile) throws Exception{
+        User 로그인유저 = userService.이메일로유저찾기(email);
+        userService.프로필사진업로드(로그인유저, imageFile, uploadPath);
+        return ResponseEntity.ok(Map.of("message","프로필 사진 저장 완료"));
+    }
+
+    /*
     // POST /user/profile/edit
     // profile.jsp 의 버튼 onclick → fetch() → JSON 받아서 성공 메세지 표시
     @PostMapping("/user/profile/edit")
@@ -83,4 +123,5 @@ public class UserApiController {
         session.setAttribute("loginUser", 최신유저);
         return Map.of("msg", "정보가 수정되었습니다.");  // 힌트: 성공 메세지 작성
     }
+     */
 }
